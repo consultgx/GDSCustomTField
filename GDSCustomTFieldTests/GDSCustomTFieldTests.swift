77555e72b9041485b1831d279ed4e0e9bace3d95
GautamD <gdsboard@gmail.com>
2016-05-17 15:36:43 -0400

    Initial Commit

---
+++ GDSCustomTFieldTests/GDSCustomTFieldTests.swift
@@ -0,0 +1,36 @@
+//
+//  GDSCustomTFieldTests.swift
+//  GDSCustomTFieldTests
+//
+//  Created by Gautam Dhall on 5/17/16.
+//  Copyright © 2016 GautamD. All rights reserved.
+//
+
+import XCTest
+@testable import GDSCustomTField
+
+class GDSCustomTFieldTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
